e3903063226f3ad13ad07ecf0e7f9f250c2577d1
alse0722 <alexeyserebruakov@yandex.ru>
2022-09-18 21:36:48 +0400

    fix ex 2

--- try2.cpp
+++ try2.cpp
@@ -4,26 +4,42 @@
 
 using namespace std;
 
-int phi (int n) {
+int phi(int n)
+{
+    vector<int> del;
+    int res(n), ns(n);
 
-	int res = n;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0)
+        {
 
-	for (int i = 2; i * i <= n; ++i)
-		if (n % i == 0) {
-			
+            printf("[%d] --> простой делитель числа %d\n", i, n);
+
+            del.push_back(i);
+
             while (n % i == 0)
                 n /= i;
 
-			res -= res / i;
-		}
+            res -= res / i;
+        }
 
-	if (n > 1)
-		res -= res / n;
-        
-	return res;
+    if (n > 1)
+    {
+        res -= res / n;
+        printf("[%d] --> простой делитель числа %d\n", n, n);
+        del.push_back(n);
+    }
+
+    printf("\nФункция эйлера выглядит следующим образом:\n phi(%d) = %d ", ns, ns);
+    for (int i = 0; i < del.size(); i++)
+        printf("* (1 - 1/%d) ", del[i]);
+    printf("\n");
+
+    return res;
 }
 
-int main(){
+int main()
+{
 
     printf("\nЧисло элементов в Z(*,n) вычисляется с помощью функции Эйлера\n");
     printf("\nНайдем порядок группы Z(*,n):\n");
@@ -32,7 +48,9 @@ int main(){
     int n;
     scanf("%d", &n);
 
-    printf("Порядок группы: %d\n", phi(n));
+    printf("\n");
+
+    printf("\nПорядок группы: %d\n", phi(n));
 
     return 0;
 }
