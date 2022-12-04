22f613d2dd78910fed00c4e06a518438edc2cdda
alse0722 <alexeyserebruakov@yandex.ru>
2022-12-05 00:49:14 +0400

    ex5 done

--- n5/ex5_1.cpp
+++ n5/ex5_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<string.h>
+#include <string.h>
 #include <vector>
 #include <cstdlib>
 #include <iostream>
@@ -35,14 +35,15 @@ struct host
     vector<packet> received_messages;
 };
 
-struct hacker {
+struct hacker
+{
     string name;
     string real_ip;
     int encrypt_algo;
     int decrypt_algo;
     host fake_host_a;
     host fake_host_b;
-    vector <packet> obtained_data;
+    vector<packet> obtained_data;
 };
 
 typedef pair<host, host> host_pair;
@@ -71,7 +72,8 @@ host get_host()
     return host;
 }
 
-hacker get_hacker(host a, host b) {
+hacker get_hacker(host a, host b)
+{
     hacker hacker;
 
     printf("\n_____________[Initialize hacker]_____________\n");
@@ -120,65 +122,73 @@ host_pair start_session(host initiator, host affector)
     affector.open_sessions.push_back(new_session_affect);
 
     printf("\n[info] Session [%s] <---> [%s] is UP!\n",
-        new_session_init.dest_ip.c_str(),
-        new_session_affect.dest_ip.c_str());
+           new_session_init.dest_ip.c_str(),
+           new_session_affect.dest_ip.c_str());
 
     return make_pair(initiator, affector);
 }
 
 host_pair drop_session(host initiator, host affector)
 {
-
     vector<session> temp;
-    session new_affector, new_initiator;
-
-    new_affector.dest_encrypt_algo = initiator.encrypt_algo;
-    new_affector.dest_ip = initiator.ip;
-    new_affector.enabled = false;
-
-    new_initiator.dest_encrypt_algo = affector.encrypt_algo;
-    new_initiator.dest_ip = affector.ip;
-    new_initiator.enabled = false;
+    session dropped;
 
     temp = {};
-    for (auto s : initiator.open_sessions)
-        if (s.dest_ip == initiator.ip)
-            temp.push_back(s);
+    for (auto e : initiator.open_sessions)
+        if (e.dest_ip == affector.ip)
+        {
+            dropped = e;
+            dropped.enabled = false;
+            temp.push_back(dropped);
+        }
+        else
+            temp.push_back(e);
 
     initiator.open_sessions = temp;
 
     temp = {};
-    for (auto s : affector.open_sessions)
-        if (s.dest_ip == affector.ip)
-            temp.push_back(s);
+    for (auto e : affector.open_sessions)
+        if (e.dest_ip == initiator.ip)
+        {
+            dropped = e;
+            dropped.enabled = false;
+            temp.push_back(dropped);
+        }
+        else
+            temp.push_back(e);
 
     affector.open_sessions = temp;
 
     printf("\n[info] Session [%s] <---> [%s] is DOWN!\n",
-        initiator.ip.c_str(),
-        affector.ip.c_str());
-
-    initiator.open_sessions.push_back(new_initiator);
-    affector.open_sessions.push_back(new_initiator);
+           initiator.ip.c_str(),
+           affector.ip.c_str());
 
     return make_pair(initiator, affector);
 }
 
-host_pair interrupt_session(host initiator, host affector, string new_ip, int new_algo) {
+host_pair interrupt_session(host initiator, host affector, hacker hacker)
+{
 
     host fake_host(initiator);
+    host_pair temp;
 
-    drop_session(fake_host, affector);
+    temp = drop_session(fake_host, affector);
+    fake_host = temp.first;
+    affector = temp.second;
 
-    fake_host.ip = new_ip;
-    fake_host.encrypt_algo = new_algo;
+    fake_host.ip = hacker.real_ip;
+    fake_host.encrypt_algo = hacker.encrypt_algo;
+    fake_host.decrypt_algo = hacker.decrypt_algo;
 
-    start_session(fake_host, initiator);
+    temp = start_session(fake_host, affector);
+    fake_host = temp.first;
+    affector = temp.second;
 
     return make_pair(fake_host, affector);
 }
 
-bool check_session(host source, host destination) {
+bool check_session(host source, host destination)
+{
     bool src_up(false), dst_up(false);
 
     for (auto session : source.open_sessions)
@@ -192,48 +202,22 @@ bool check_session(host source, host destination) {
     return src_up && dst_up;
 }
 
-/*host_pair start_fake_session(host initiator, host affector, host hacker)
+packet format_message(host source, host destinaton)
 {
-
-    host fake_initiator, fake_affector;
-    session fake_init_session, fake_affect_session;
-
-    fake_init_session.dest_ip = initiator.ip;
-    fake_init_session.dest_encrypt_algo = initiator.encrypt_algo;
-    fake_init_session.enabled = true;
-
-    fake_affect_session.dest_ip = affector.ip;
-    fake_affect_session.dest_encrypt_algo = affector.encrypt_algo;
-    fake_affect_session.enabled = true;
-
-    fake_initiator.ip = initiator.ip;
-    fake_initiator.name = initiator.name;
-    fake_initiator.encrypt_algo = initiator.encrypt_algo;
-    fake_initiator.decrypt_algo = -1;
-    fake_initiator.open_sessions.push_back(fake_init_session);
-
-    fake_affector.ip = affector.ip;
-    fake_affector.name = affector.name;
-    fake_affector.encrypt_algo = affector.encrypt_algo;
-    fake_affector.decrypt_algo = -1;
-    fake_affector.open_sessions.push_back(fake_affect_session);
-
-    drop_session(fake_initiator, affector);
-    drop_session(fake_affector, initiator);
-
-    fake_initiator.encrypt_algo = hacker.encrypt_algo;
-    fake_initiator.decrypt_algo = hacker.decrypt_algo;
-    fake_affector.encrypt_algo = hacker.encrypt_algo;
-    fake_affector.encrypt_algo = hacker.decrypt_algo;
-
-    start_session(fake_initiator, affector);
-    start_session(fake_affector, initiator);
-
-    return make_pair(fake_initiator, fake_affector);
-}*/
-
-packet format_message(host source, host destinaton, string message) {
     packet pkg;
+
+    char alpha[73] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*<>";
+    char msg[10] = {};
+    int ind;
+    string message;
+
+    for (int i = 0; i < 10; i++)
+    {
+        ind = rand() % 72;
+        msg[i] = alpha[ind];
+    }
+
+    message = string(msg, 10);
 
     pkg.source_ip = source.ip;
     pkg.destination_ip = destinaton.ip;
@@ -243,31 +227,59 @@ packet format_message(host source, host destinaton, string message) {
     return pkg;
 }
 
-host_pair send_message(host source, host destination, string message) {
+host_pair send_message(host source, host destination)
+{
 
-    packet pkg(format_message(source, destination, message));
+    packet pkg(format_message(source, destination));
 
-    if (check_session(source, destination)) {
+    if (check_session(source, destination))
+    {
         pkg.status = "SENT";
         source.sent_messages.push_back(pkg);
         pkg.status = "RECEIVED";
         destination.received_messages.push_back(pkg);
     }
-    else {
+    else
+    {
         pkg.status = "SENDING FAILURE";
         source.sent_messages.push_back(pkg);
     }
     return make_pair(source, destination);
 }
 
-host decrypt_messages(host host) {
-    vector <packet> processed;
+host_pair reply_last_message(host source, host fake_source, host destination)
+{
+    packet msg = source.received_messages.back();
+    packet fake(format_message(fake_source, destination));
+
+    fake.message = msg.message;
+    fake.source_ip = msg.source_ip;
+
+    if (check_session(fake_source, destination))
+    {
+        fake.status = "RECEIVED";
+        destination.received_messages.push_back(fake);
+    }
+    else
+    {
+        fake.status = "SENDING FAILURE";
+        fake_source.sent_messages.push_back(fake);
+    }
+
+    return make_pair(fake_source, destination);
+}
+
+host decrypt_messages(host host)
+{
+    vector<packet> processed;
     for (auto message : host.received_messages)
-        if (message.encrypted_with == host.encrypt_algo) {
+        if (message.encrypted_with == host.encrypt_algo)
+        {
             message.status = "DECRYPTION SUCCESS";
             processed.push_back(message);
         }
-        else {
+        else
+        {
             message.status = "DECRYPTION FAILURE";
             message.message = "*****************";
             processed.push_back(message);
@@ -276,6 +288,17 @@ host decrypt_messages(host host) {
     host.received_messages = processed;
 
     return host;
+}
+
+hacker collect_data(hacker hacker)
+{
+    for (auto e : hacker.fake_host_a.received_messages)
+        hacker.obtained_data.push_back(e);
+
+    for (auto e : hacker.fake_host_b.received_messages)
+        hacker.obtained_data.push_back(e);
+
+    return hacker;
 }
 
 void print_host(host host)
@@ -291,8 +314,8 @@ void print_host(host host)
     if (!host.open_sessions.empty())
         for (auto connection : host.open_sessions)
             printf(connection.enabled ? "|    [UP]   %s : %d\n" : "|    [DOWN] %s : %d\n",
-                connection.dest_ip.c_str(),
-                connection.dest_encrypt_algo);
+                   connection.dest_ip.c_str(),
+                   connection.dest_encrypt_algo);
     else
         printf("|    ! No connections detected\n");
 
@@ -300,49 +323,198 @@ void print_host(host host)
     if (!host.sent_messages.empty())
         for (auto msg : host.sent_messages)
             printf("|    Sent {%s} to [%s:%d] --> %s\n",
-                msg.message.c_str(),
-                msg.destination_ip.c_str(),
-                msg.encrypted_with,
-                msg.status.c_str());
+                   msg.message.c_str(),
+                   msg.destination_ip.c_str(),
+                   msg.encrypted_with,
+                   msg.status.c_str());
     else
         printf("|    ! Sent 0 messages\n");
 
     if (!host.received_messages.empty())
         for (auto msg : host.received_messages)
             printf("|    Received {%s} from [%s:%d] --> %s\n",
-                msg.message.c_str(),
-                msg.source_ip.c_str(),
-                msg.encrypted_with,
-                msg.status.c_str());
+                   msg.message.c_str(),
+                   msg.source_ip.c_str(),
+                   msg.encrypted_with,
+                   msg.status.c_str());
     else
         printf("|    ! Received 0 messages\n");
 
     printf("|______________________________________[%s]_______________________________________\n", host.name.c_str());
 }
 
-void print_hacker(hacker hacker) {
-    printf("\n ======================================[%s]======================================\n", hacker.name.c_str());
+void print_hacker(hacker hacker)
+{
+    printf("\n ======================================[%s]=======================================\n", hacker.name.c_str());
     printf("| [Name]    --> %s\n", hacker.name.c_str());
     printf("| [IP]      --> %s\n", hacker.real_ip.c_str());
     printf("| [Encrypt] --> %d\n", hacker.encrypt_algo);
     printf("| [Decrypt] --> %d\n", hacker.decrypt_algo);
-    printf("| [Fake hosts]\n");
-
-    print_host(hacker.fake_host_a);
-    print_host(hacker.fake_host_b);
 
     printf("| [Obtained data]\n");
     if (!hacker.obtained_data.empty())
         for (auto msg : hacker.obtained_data)
             printf("|    Received {%s} from [%s:%d] --> %s\n",
-                msg.message.c_str(),
-                msg.source_ip.c_str(),
-                msg.encrypted_with,
-                msg.status.c_str());
+                   msg.message.c_str(),
+                   msg.source_ip.c_str(),
+                   msg.encrypted_with,
+                   msg.status.c_str());
     else
         printf("|    ! Received 0 messages\n");
 
-    printf("\n|======================================[%s]======================================\n", hacker.name.c_str());
+    // printf("| [Fake hosts]\n");
+    //
+    // print_host(hacker.fake_host_a);
+    // print_host(hacker.fake_host_b);
+
+    printf("\n|======================================[%s]=======================================\n", hacker.name.c_str());
+}
+
+void normal_mode()
+{
+    host a, b;
+    host_pair temp;
+
+    // system("color 20");
+    printf("\n[sys] Mode: Normal\n");
+    printf("\n[sys] Initialize hosts");
+    // system("color 70");
+
+    a = get_host();
+    b = get_host();
+
+    // system("color 20");
+    printf("\n[sys] Start session between hosts %s and %s\n", a.name.c_str(), b.name.c_str());
+    // system("color 70");
+
+    temp = start_session(a, b);
+    a = temp.first;
+    b = temp.second;
+
+    print_host(a);
+    print_host(b);
+
+    // system("color 20");
+    printf("\n[sys] Message excanges between hosts %s and %s\n", a.name.c_str(), b.name.c_str());
+    // system("color 70");
+
+    temp = send_message(a, b);
+    a = temp.first;
+    b = decrypt_messages(temp.second);
+
+    temp = send_message(b, a);
+    b = temp.first;
+    a = decrypt_messages(temp.second);
+
+    print_host(a);
+    print_host(b);
+
+    // system("color 20");
+    printf("\n[sys] Close session between hosts %s and %s\n", a.name.c_str(), b.name.c_str());
+    // system("color 70");
+
+    temp = drop_session(a, b);
+    a = temp.first;
+    b = temp.second;
+
+    print_host(a);
+    print_host(b);
+
+    // system("color 20");
+    printf("\n[sys] END\n");
+    // system("color 70");
+}
+
+void hacked_session_mode()
+{
+    host a, b;
+    hacker h;
+    host_pair temp;
+    pair<hacker, host> reply;
+
+    // system("color 20");
+    printf("\n[sys] Mode: Hacked session\n");
+    printf("\n[sys] Initialize hosts");
+    // system("color 70");
+
+    a = get_host();
+    b = get_host();
+
+    // system("color 20");
+    printf("\n[sys] Start session between hosts %s and %s\n", a.name.c_str(), b.name.c_str());
+    // system("color 70");
+
+    temp = start_session(a, b);
+    a = temp.first;
+    b = temp.second;
+
+    // print_host(a);
+    // print_host(b);
+
+    // system("color 20");
+    printf("\n[sys] Initialize hacker");
+    // system("color 70");
+
+    h = get_hacker(a, b);
+    print_hacker(h);
+
+    // system("color 20");
+    printf("\n[sys] Hacker interrupts the session");
+    // system("color 70");
+
+    temp = interrupt_session(h.fake_host_b, a, h);
+    h.fake_host_b = temp.first;
+    a = temp.second;
+
+    temp = interrupt_session(h.fake_host_a, b, h);
+    h.fake_host_a = temp.first;
+    b = temp.second;
+
+    print_host(a);
+    print_host(b);
+    print_hacker(h);
+
+    // system("color 20");
+    printf("\n[sys] Message excanges between hosts %s and %s\n", a.name.c_str(), b.name.c_str());
+    // system("color 70");
+
+    temp = send_message(a, h.fake_host_b);
+    a = temp.first;
+    h.fake_host_b = decrypt_messages(temp.second);
+
+    temp = reply_last_message(h.fake_host_b, h.fake_host_a, b);
+    h.fake_host_a = temp.first;
+    b = decrypt_messages(temp.second);
+
+    temp = send_message(b, h.fake_host_a);
+    b = temp.first;
+    h.fake_host_a = decrypt_messages(temp.second);
+
+    temp = reply_last_message(h.fake_host_a, h.fake_host_b, a);
+    h.fake_host_b = temp.first;
+    a = decrypt_messages(temp.second);
+
+    h = collect_data(h);
+
+    // print_host(a);
+    // print_host(b);
+    // print_hacker(h);
+
+    // system("color 20");
+    printf("\n[sys] Close all sessions \n");
+    // system("color 70");
+
+    temp = drop_session(h.fake_host_a, b);
+    h.fake_host_a = temp.first;
+    b = temp.second;
+
+    temp = drop_session(h.fake_host_b, a);
+    h.fake_host_b = temp.first;
+    a = temp.second;
+
+    print_host(a);
+    print_host(b);
+    print_hacker(h);
 }
 
 int main()
@@ -350,43 +522,8 @@ int main()
     setlocale(0, "");
     srand(time(NULL));
 
-    host_pair new_session, old_session;
-    string str1("test1"), str2("test2");
-
-    // host a(get_host()), b(get_host()), c(get_host());
-    host a, b, c;
-    a.name = "A";
-    a.ip = "192.168.0.174";
-    a.decrypt_algo = 1111;
-    a.encrypt_algo = 9999;
-    a.open_sessions = {};
-
-    b.name = "B";
-    b.ip = "192.168.0.111";
-    b.decrypt_algo = 3333;
-    b.encrypt_algo = 2222;
-    b.open_sessions = {};
-
-    print_host(a);
-    print_host(b);
-
-    new_session = start_session(a, b);
-    a = new_session.first;
-    b = new_session.second;
-    print_host(a);
-    print_host(b);
-
-    new_session = send_message(a, b, str1);
-    a = new_session.first;
-    b = decrypt_messages(new_session.second);
-    print_host(a);
-    print_host(b);
-
-    //old_session = drop_session(a, b);
-    //a = old_session.first;
-    //b = old_session.second;
-    //print_host(a);
-    //print_host(b);
+    // normal_mode();
+    hacked_session_mode();
 
     return 0;
 }
